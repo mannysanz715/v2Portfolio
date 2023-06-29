a81ebd29e8fe37ff8ca96fb8a88934265ce0ecb0
mannysanz715 <mannysanz715@gmail.com>
2023-06-28 19:45:23 -0700

    Add text to Window. Still fixing positioning

--- src/components/MacScreen/MacScreen.jsx
+++ src/components/MacScreen/MacScreen.jsx
@@ -1,5 +1,7 @@
+
 import './MacScreen.css'
 function MacScreen () {
+  const text = [['<>', '#'], ['//Click-Able Components','#'],['<Header />', '#'], ['<Projects />', '#projects'], ['<About Me />', '#about-me'], ['<Technologies />', '#technologies'], ['<Contact Me/>', '#contact-me'], ['</>', '#']]
   return (
     <>
       <div className='whole-window'>
@@ -8,7 +10,13 @@ function MacScreen () {
           <div className='round yellow'></div>
           <div className='round green'></div>
         </div>
-
+        <div className='text-container'>
+          {
+            text.map((line, idx)=>
+            <a key={idx} href={line[1]} className='header-link'id={`line-${idx}`}>{line[0]}</a>
+              )
+          }
+        </div>
       </div>
     </>
 )
