0548d42aa147e42357d602efbdf6fe0ad6656cbc
mannysanz715 <mannysanz715@gmail.com>
2023-06-29 19:29:02 -0700

    Style macbook window on landing screen

--- src/components/MacScreen/MacScreen.jsx
+++ src/components/MacScreen/MacScreen.jsx
@@ -1,7 +1,9 @@
 
 import './MacScreen.css'
 function MacScreen () {
-  const text = [['<>', '#'], ['//Click-Able Components','#'],['<Header />', '#'], ['<Projects />', '#projects'], ['<About Me />', '#about-me'], ['<Technologies />', '#technologies'], ['<Contact Me/>', '#contact-me'], ['</>', '#']]
+  const text = [['//Click-Able Components', '#'],['<Header />', '#'], ['<Projects />', '#projects'], ['<About Me />', '#about-me'], ['<Technologies />', '#technologies'], ['<Contact Me/>', '#contact-me']]
+  const text1 = '<>'
+  const text3 = '</>'
   return (
     <>
       <div className='whole-window'>
@@ -11,11 +13,15 @@ function MacScreen () {
           <div className='round green'></div>
         </div>
         <div className='text-container'>
-          {
-            text.map((line, idx)=>
-            <a key={idx} href={line[1]} className='header-link'id={`line-${idx}`}>{line[0]}</a>
+          <p className='open-rf'>{text1}</p>
+          <div className='text-content'>
+            {
+              text.map((line, idx)=>
+              <a key={idx} href={line[1]} className='header-link'id={`line-${idx}`}>{line[0]}</a>
               )
-          }
+            } 
+          </div>
+            <p className='close-rf'>{text3}</p>
         </div>
       </div>
     </>
