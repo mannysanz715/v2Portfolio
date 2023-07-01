01cfbaa6365ddd2a8de2ba9ca60e8e3b6c51d847
mannysanz715 <mannysanz715@gmail.com>
2023-06-30 22:28:02 -0700

    Create most of the project data

--- src/components/ProjectsSection/ProjectsSection.jsx
+++ src/components/ProjectsSection/ProjectsSection.jsx
@@ -1,17 +1,13 @@
 import ProjectCard from '../ProjectCard/ProjectCard'
 import './ProjectsSection.css'
-
+import projects from '../../data/projects.js'
 function ProjectsSection(){
-  const project = {
-    image: 'img',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum dolorum vel inventore iusto in unde optio nemo. Iste maiores maxime harum, sapiente veniam tempora dignissimos illo, praesentium alias ut qui Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum dolorum vel inventore iusto in unde optio nemo. Iste maiores maxime harum, sapiente veniam tempora dignissimos illo, praesentium alias ut qui!',
-    title: 'Project 1',
-  }
   return (
       <div className='projects-section' id='projects'>
         <h1 className='projects-section-title'>Projects</h1>
-        <ProjectCard title={project.title} idx={0} description={project.description}/>
-        <ProjectCard title={project.title} idx={1} description={project.description}/>
+        {projects.map((project, idx)=>
+          <ProjectCard key={idx} image={project.image} liveLink={project.liveLink} codeLink={project.codeLink} title={project.title} idx={idx} description={project.description}  />
+        )}
       </div>
 )
 }
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,4 +1,4 @@
-export const projects = [
+const projects = [
   // {
   //   title: '',
   //   description : '',
@@ -8,9 +8,39 @@ export const projects = [
   // },
   {
     title: "Grace's Driving School",
-    description : '',
-    image : '',
-    codeLink : '',
-    liveLink : '',
+    description : 'asd ajksdnak sjdna skdj askdj naskdjn askdj naskdnjasdn asd,masldamnsalsjdn aksdjn asd lasdm alsd naslkdjn asdaskdjn asd',
+    image : '/assets/projectImages/drivingschool.png',
+    codeLink : 'https://github.com/mannysanz715/BestDrivingTeacherFront',
+    liveLink : 'https://gracesdrivingschool.com',
+  },  
+  {
+    title: 'Exchange-O-Gram',
+    description : 'Exchange-O-Gram is a social media photo-sharing application. After signing up, a user can upload photos and display them on the feed page and their profile page. The user can also view other users photos on the feed page and like and comment on each post. If a user wants to message another user directly, they can send and respond to messages from their profile page. Exchange-O-Gram makes it easy to keep up with your friends lives through photo sharing!',
+    image : '/assets/projectImages/exchange-o-gram.png',
+    codeLink : 'https://github.com/mannysanz715/exchangeagram-front-end/tree/main',
+    liveLink : 'https://exchangeagram.netlify.app/',
   },
+  {
+    title: 'What to Watch?',
+    description : 'Ever get stuck on what you want to watch at night? Do you ever waste the night searching for the right movie, just to end up not watching anything because you spent too much time on deciding? Welcome to "What to Watch?", where you can get a movie reccomendation thrown at you based on the genre you are in the mood for!',
+    image : '/assets/projectImages/whattowatch.png',
+    codeLink : 'https://github.com/mannysanz715/movie-rec-front',
+    liveLink : 'https://movie-rec-app.netlify.app/',
+  },
+  {
+    title: 'ConnectFOR',
+    description : 'ConnectFOR is a Two Player game that requires an imense amount of skill. The cirlces in the board are click-able, dropping a ConnectFOR piece into the lowest open slot on the board. Each player is assigned a color. Player 1 is red : Player 2 yellow. The objective is to take turns with your opponent, dropping pieces into slots. The first person to connect 4 of them in a row is the winner. BE WARNED. THIS GAME WILL RUIN FRIENDSHIPS!',
+    image : '/assets/projectImages/connect4.png',
+    codeLink : 'https://github.com/mannysanz715/connect4',
+    liveLink : 'https://connect4ms.netlify.app/',
+  },
+  {
+      title: 'Cakes by Murr (Cake Calculator)',
+      description : 'Still in the works...',
+      image : '/assets/projectImages/cakecalculator.png',
+      codeLink : 'https://github.com/mannysanz715/Cake-Calculator-Front',
+      liveLink : '#',
+    },
 ]
+
+export default projects
